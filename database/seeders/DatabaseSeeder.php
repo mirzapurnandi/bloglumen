c7b09227dfe44cdd51cd685e94b40f7e94ed20dc
mirza purnandi <mirza.purnandi@gmail.com>
2021-10-30 23:30:29 +0700

    tambah Seeder Tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
+            TagSeeder::class,
         ]);
         // $this->call('UsersTableSeeder');
     }
---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tags')->insert([
+            ['name' => 'Categori Satu', 'slug' => 'categori-satu'],
+            ['name' => 'Categori Dua', 'slug' => 'categori-dua'],
+            ['name' => 'Categori Tiga', 'slug' => 'categori-tiga'],
+            ['name' => 'Categori Empat', 'slug' => 'categori-empat'],
+            ['name' => 'Categori Lima', 'slug' => 'categori-lima'],
+            ['name' => 'Categori Enam', 'slug' => 'categori-enam'],
+        ]);
+    }
+}
